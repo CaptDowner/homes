edba4f0c63f82a382929757510d5a6b405e48d5c
CaptDowner <captdowner@comcast.net>
2012-04-01 18:03:30 -0700

    Added Rack::ContentLength.

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module Homes
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
+#    config.middleware.use Rack::ContentLength
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
